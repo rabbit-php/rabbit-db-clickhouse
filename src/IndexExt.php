3e640d9bf752d01ed9f1fec0a2efdafb2c6e2328
albert <63851587@qq.com>
2019-05-28 15:49:30 +0800

    update

---
+++ src/IndexExt.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace rabbit\db\clickhouse;
+
+use rabbit\activerecord\ActiveRecord;
+use rabbit\db\DBHelper;
+use rabbit\db\Query;
+
+/**
+ * Class IndexExt
+ * @package rabbit\db\mysql
+ */
+class IndexExt
+{
+    /**
+     * @param Query $query
+     * @param array $filter
+     * @param int $page
+     * @return array
+     */
+    public static function index(Query $query, array $filter = [], int $page = 0): array
+    {
+        return DBHelper::SearchList($query, $filter, $page);
+    }
+}
