7b4f2ad542e238dc6a38680d534208ce40844421
conlin <Conlin_huang@patazon.net>
2019-12-10 21:53:23 +0800

    fix clickhouse tcp connection

--- src/MakeCKConnection.php
+++ src/MakeCKConnection.php
@@ -4,6 +4,9 @@
 namespace rabbit\db\clickhouse;
 
 use rabbit\core\ObjectFactory;
+use rabbit\exception\InvalidConfigException;
+use rabbit\helper\ArrayHelper;
+use function Swlib\Http\parse_query;
 
 class MakeCKConnection
 {
@@ -13,13 +16,10 @@ class MakeCKConnection
      */
     public static function addConnection(string $class, string $name, string $dsn, array $config = null): string
     {
-        $driver = parse_url($dsn, PHP_URL_SCHEME);
+        $urlArr = parse_url($dsn);
+        $driver = $urlArr['scheme'];
         /** @var Manager $manager */
-        if (in_array($driver, ['http', 'https'])) {
-            $manager = getDI('clickhouse');
-        } else {
-            $manager = getDI('click');
-        }
+        $manager = getDI($driver);
         if (!$manager->hasConnection($name)) {
             $conn = [
                 'class' => $class,
@@ -30,7 +30,37 @@ class MakeCKConnection
                     $conn[$key] = $value;
                 }
             }
-            $manager->addConnection([$name => ObjectFactory::createObject($conn, [], false)]);
+            if ($driver === 'clickhouse') {
+                $manager->addConnection([$name => ObjectFactory::createObject($conn, [], false)]);
+            } elseif ($driver === 'click') {
+                $poolConfig = [
+                    'class' => \rabbit\db\pool\PdoPoolConfig::class,
+                ];
+                [
+                    $poolConfig['minActive'],
+                    $poolConfig['maxActive'],
+                    $poolConfig['maxWait'],
+                    $poolConfig['maxReconnect']
+                ] = ArrayHelper::getValueByArray(parse_query($urlArr['query']), [
+                    'min',
+                    'max',
+                    'wait',
+                    'retry'
+                ], null, [
+                    5,
+                    5,
+                    0,
+                    3
+                ]);
+                $conn['pool'] = ObjectFactory::createObject([
+                    'class' => \rabbit\db\pool\PdoPool::class,
+                    'poolConfig' => ObjectFactory::createObject($poolConfig, [], false)
+                ], [], false);
+                $manager->addConnection([$name => $conn]);
+            } else {
+                throw new InvalidConfigException("Not support driver $driver");
+            }
+
         }
         return $driver;
     }
