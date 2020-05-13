4327afaabe0d15ba2a0d47c8e464ad04a9eb1b25
albert <amuluowin@163.com>
2020-05-13 14:25:10 +0800

    remove unuse code

--- src/Manager.php
+++ src/Manager.php
@@ -8,11 +8,6 @@
 
 namespace rabbit\db\clickhouse;
 
-use rabbit\core\ObjectFactory;
-use rabbit\db\ConnectionInterface;
-use rabbit\db\Exception;
-use rabbit\helper\ArrayHelper;
-
 /**
  * Class Manager
  * @package rabbit\db\clickhouse
@@ -21,8 +16,6 @@ class Manager
 {
     /** @var PdoPool[] */
     private $connections = [];
-    /** @var array */
-    private $yamlList = [];
 
     /**
      * Manager constructor.
@@ -52,10 +45,7 @@ class Manager
     public function getConnection(string $name = 'db'): ?Connection
     {
         if (!isset($this->connections[$name])) {
-            if (empty($this->yamlList)) {
-                return null;
-            }
-            $this->createByYaml();
+            return null;
         }
         return $this->connections[$name];
     }
@@ -67,29 +57,5 @@ class Manager
     public function hasConnection(string $name): bool
     {
         return isset($this->connections[$name]);
-    }
-
-    private function createByYaml(): void
-    {
-        foreach ($this->yamlList as $fileName) {
-            foreach (yaml_parse_file($fileName) as $name => $dbconfig) {
-                if (!isset($dbconfig['class']) || !class_exists($dbconfig['class']) || !$dbconfig['class'] instanceof ConnectionInterface) {
-                    $dbconfig['class'] = Connection::class;
-                }
-                if (!isset($dbconfig['dsn'])) {
-                    throw new Exception("The dsn must be set current class in $fileName");
-                }
-                $conn = [
-                    'class' => $dbconfig['class'],
-                    'dsn' => $dbconfig['dsn'],
-                ];
-                if (is_array(ArrayHelper::getValue($dbconfig, 'config'))) {
-                    foreach ($config as $key => $value) {
-                        $conn[$key] = $value;
-                    }
-                }
-                $this->connections[$name] = ObjectFactory::createObject($conn, [], false);
-            }
-        }
     }
 }
